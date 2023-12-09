f755fd3bda8ac2542db8cb4ebcf8dc71727d84df
Felipe chagas <felipereis9998@aluno.saojudas.br>
2023-12-08 22:25:19 -0300

    fix: ajuste services

--- src/app/services/coment/coment.service.spec.ts
+++ src/app/services/coment/coment.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ComentService } from './coment.service';
+import { ComentService } from '../coment/coment.service';
 
 describe('ComentService', () => {
   let service: ComentService;
