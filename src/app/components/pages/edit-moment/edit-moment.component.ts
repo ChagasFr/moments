674753a2fb7c0df286f6c4433a987bb684beefe6
Felipe chagas <felipereis9998@aluno.saojudas.br>
2023-12-12 22:16:53 -0300

    iniciacao edit-moment

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { MomentService } from './../../../services/moment/moment.service';
+import { Moment } from '../../../moment';
 
 @Component({
   selector: 'app-edit-moment',
   standalone: true,
   imports: [],
   templateUrl: './edit-moment.component.html',
-  styleUrl: './edit-moment.component.css'
+  styleUrl: './edit-moment.component.css',
 })
-export class EditMomentComponent {
+export class EditMomentComponent implements OnInit {
+  moment!: Moment;
+  btnText: string = 'Editar';
 
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.momentService.getMoment(id).subscribe((item) => {
+      this.moment = item.data;
+    });
+  }
 }
